7912ca251df8ef3f3f1cea88f08fc4761b9a2dbc
Anastasia <84379046+az-journal@users.noreply.github.com>
2021-11-22 10:32:13 +0400

    Update Barrels.sol

--- Barrels.sol
+++ Barrels.sol
@@ -32,4 +32,22 @@ contract Barrels is ERC721Enumerable, Ownable {
     for (uint256 i = 0; i < _mintAmount; i++) { 
       _safeMint(_to, supply + i);
     }
+    
+    function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
+  }
   }
