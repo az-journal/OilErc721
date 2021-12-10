6ac89bf3dcb7d1ade77c4b55d493077d59db8ad7
Anastasia <84379046+az-journal@users.noreply.github.com>
2021-12-10 13:11:13 +0500

    Update Barrels.sol

--- Barrels.sol
+++ Barrels.sol
@@ -1,54 +1,27 @@
-pragma solidity ^0.8.0;
-// SPDX-License-Identifier: GPL-3.0
-
-
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Barrels is ERC721Enumerable, Ownable {
-  using Strings for uint256;
+contract Barrels is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-  string public baseURI;
-  string public baseExtension = ".json";
-  
-
-   constructor(
-    string memory _name,
-    string memory _symbol,
-    string memory _initBaseURI
-  ) ERC721(_name, _symbol) {
-    setBaseURI(_initBaseURI);
-  }
-
- 
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
-  }
-
-
-  function mint(address msg.sender, uint256 _mintAmount) public payable {
-    uint256 supply = totalSupply();
-    for (uint256 i = 0; i < _mintAmount; i++) { 
-      _safeMint(msg.sender, supply + i);
+    constructor(string memory tokenName, string memory symbol) ERC721(tokenName, symbol) {
+        _setBaseURI("ipfs://");
     }
-    
-    function tokenURI(uint256 tokenId) 
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
 
-    string memory currentBaseURI = _baseURI();
-    return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
-        : "";
-  }
-  }
+    function mintToken(address owner, string memory metadataURI)
+    public
+    returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 id = _tokenIds.current();
+        _safeMint(owner, id);
+        _setTokenURI(id, metadataURI);
+
+        return id;
+    }
+}
